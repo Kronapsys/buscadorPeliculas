128e78f366be9b6a396822b220ee2389b56911aa
Kronapsys <rsxvii@gmail.com>
2021-02-17 22:58:19 +0100

    Implementada funcion busqueda por ID

--- js/main.js
+++ js/main.js
@@ -1,50 +1,64 @@
 //funciones
-let valorTitulo = document.getElementById("buscaTitulos");
-let valorID = document.getElementById("buscaIDs");
+let valor = document.getElementById('buscador');
+let valorId = document.getElementById('buscadorID');
 
 //Recursos API
-let key = "c0b6dea31a9d647a6b7d1eafa59bacaa";
-let recurso= "search";
-let criterio = "movie";
-let id = "464052";
-//let external_ids = "tt7126948";
-let base_url = `http://api.themoviedb.org/3/${recurso}`;
-
+let key = 'be761d96072c0447097947c70d2686b6';
+let recurso = 'search';
+let criterio = 'movie';
+let base_url = `http://api.themoviedb.org/3/`;
 
 const call = async (url) => {
     let res = await axios.get(url);
     
-    if(!res.data.results){
-        error = new Error("La url era incorrecta");
-        return error;
+    if (res.data.results){
+        return res.data.results;
+
     }
-    return res.data.results;
+    if (res.data.title){
+        return res.data;
+    }
 }
 
 const pintar = async (coleccionPintar) => {
     //Proceso para el pintado HTML de las películas
-    document.getElementById("contenedor").innerHTML = JSON.stringify(coleccionPintar);
-
+    const divPelisDomElement = document.getElementById('contenedor');
+    divPelisDomElement.innerHTML = "";
+    if (Array.isArray(coleccionPintar)) {
+        coleccionPintar.forEach(pelicula => {
+            divPelisDomElement.innerHTML += `<div id='peliculas'>
+            <img src='https://image.tmdb.org/t/p/w500${pelicula.poster_path}'></img></div>
+            <div id='textos'><h2>${pelicula.title}</h2>
+            <p>${pelicula.overview}</p>
+            </div>`
+        });
+    }
+    
+    if (coleccionPintar.original_title){
+        divPelisDomElement.innerHTML += `<div class='peliculas'>
+        <img src='https://image.tmdb.org/t/p/w500${coleccionPintar.poster_path}' class='picture'>
+        </img></div><div class='infoPelis'><h2>${coleccionPintar.original_title}</h2><p>${coleccionPintar.overview}</p></div>`
+    }
     return;
-}
-
-const buscaTitulos = async () => {
-    let query = valorTitulo.value;
-    // Construccion de la URL 
-    let url = `${base_url}/${criterio}?api_key=${key}&query=${query}`; 
-    pintar("Cargando");
-    let pelis = await call(url);
-
-    pintar(pelis);
 };
 
-const buscaIDs = async () => {
-    let external_ids = valorID.value;
-    // Construcción de la URL
-    // https://api.themoviedb.org/3/movie/464052/external_ids?api_key=c0b6dea31a9d647a6b7d1eafa59bacaa
-    let url = `http://api.themoviedb.org/3/${criterio}/${id}/${external_ids}?api_key=${key}`;
-    pintar("Cargando");
+const buscador = async () => {
+    let query = valor.value;
+    //Construccion de la URL 
+    let url = `${base_url}${recurso}/${criterio}?api_key=${key}&query=${query}`; 
+    pintar("cargando");
     let pelis = await call(url);
 
-    pintar(pelis);
+    pintar(pelis);  
+};
+
+const buscadorID = async () => {
+    let query = valorId.value;
+    
+    //Construccion de la URL 
+    let url = `${base_url}${criterio}/${query}?api_key=${key}`; 
+    pintar("cargando");
+    let pelis = await call(url);
+
+    pintar(pelis);  
 };
